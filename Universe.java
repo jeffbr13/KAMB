f82e790bb3a647271b45a3e4dbfe28c06d075c2d
Atanas Dobrev <atanas_d_d@abv.bg>
2013-02-13 01:52:43 +0000

    Added some stuff and did some fixes... needs more fixes.
    I've got some suggestions for making this better. As it is now... it's
    not gonna work out so well.

--- Universe.java
+++ Universe.java
@@ -1,16 +1,32 @@
 /**
- * A Universe object generates a game upon initialisation,
+ * A Universe object generates a game upon initialization,
  * and contains references to all the related objects, such
  * as Planets and Fleets.
  */
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Universe
 {
     private int width;
     private int height;
     
     private Fleet[] fleets; 
+    
+    //n= Number of planets.
+    public int n;
+    
+    //Used for getting random values.
+    private Random random;
+    
+    //How much do you want the distance between planets to be?
+    private int planetDistance=20;
+    
     private Planet[] planets;
     private Drawable[] drawableEntities;
+    private BufferedImage[] planetImgs;
     
     /*
      * This generates a universe of the appropriate width, height, and number
@@ -20,23 +36,60 @@ public class Universe
     {
         this.width = width;
         this.height = height;
+        n=numPlanets;
+        this.planets = new Planet[n];
         
-        this.planets = new Planet[numPlanets];
-        for (int i=0; i < numPlanets; i++) {
-            // FIXME: default values place a planet at (50,50), with a radius of 40. 
-            this.planets[i] = new Planet(50,50,40);
+        /*
+        Make an array that holds the planet images and make the 3-rd value of the planet
+        constructor be the image. NOT the radius since it's gonna be the same for all planets
+        anyway. Also all the images need to be the same size. I've named that array planetImgs.
+        It should have predetermined "values" and size depending on the number of images we have
+        and they have to be enteres manualy each. I'm not gonna outright do this until you guys agree.
+        */
+        int x,y,img,i,j;
+        for(i=0;i<n;i++)
+        {
+         //Chooses a random image for the planet.
+         img=random.nextInt(planetImgs.length);
+         do
+         {
+          //Chooses random coordinates.
+          x=random.nextInt(width-planetImgs[img].getWidth());
+          y=random.nextInt(height-planetImgs[img].getWidth());
+          for(j=0;j<i-1;j++)
+          {
+           //Checks if the current randoms are ok with all current planets one at a time:
+           if((planets[j].x-x)*(planets[j].x-x)+(planets[j].y-y)*(planets[j].y-y)<(2*planets[j].radius+planetDistance)*(2*planets[j].radius+planetDistance))
+           {
+            //If they aren't far enough from some existing planet, I stop the loop and j remains
+        	//smaller than i-1; for the first planet it will be 0<0-1 so it'll always pass.
+        	break;
+           }
+          }
+          //Checks if we reached the end of the loop and therefore the current coordinates are ok.
+         }
+         while(j<i-1);
+         //Make them take image as a third construction parameter and delete this while
+         //uncommenting the other one.
+         planets[i]=new Planet(x,y,40);
+         //planets[i]=new Planet(x,y,planetImgs[img]);
         }
-        //TODO: ensure generated planets are a sensible distance apart, and have a sensible radius.
     }
     
     public void addPlanet(Planet p)
     {
-        this.planets[planets.length] = p;
+    	//You can't add planets like this, trying to do that will give a runtime error.
+    	//We don't need to add planets (for now) anyway.
+        //this.planets[planets.length] = p;
     }
     
     public void addFleet(Fleet f)
     {
+    	/*
+    	Can't add fleets like that either. You're basically trying to change a value which is outside
+    	the array- remember an array is from 0 to it's length-1 and doesn't allow change of it's size.
         this.fleets[fleets.length] = f;
+        */
     }
 
     
