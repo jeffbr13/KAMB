de292d6943241ee98e021af94c28b84cc44b4904
Ben Jeffrey <mail@benjeffrey.net>
2013-03-02 16:19:34 +0000

    Added basic Player class

---
+++ Player.java
@@ -0,0 +1,26 @@
+
+/**
+ * @author jeffbr
+ *
+ */
+public class Player
+{ 
+    private int number;
+
+    /**
+     * Create a new player, with a number.
+     */
+    public Player(int number)
+    {
+        this.number = number;
+    }
+
+    /**
+     * @return get the Player's number
+     */
+    public int getNumber()
+    {
+        return number;
+    }
+
+}
