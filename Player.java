6dd66b04dcff0d72db7c2ee80a76e7c2f4851ab1
Atanas Dobrev <atanas_d_d@abv.bg>
2013-03-09 17:48:55 +0000

    Added static array Colors to Player.

--- Player.java
+++ Player.java
@@ -1,4 +1,9 @@
 import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 
 /**
@@ -13,6 +18,33 @@ public class Player
     /**
      * Create a new player, with the given number and a random colour.
      */
+    
+    public static Color[] colors;
+    public static int numColors;
+    
+    static
+    {
+     numColors=18;
+     colors=new Color[numColors];
+     int i;
+     Color t;
+     BufferedImage image = null;
+     for(i=0;i<numColors;i++)
+     {
+    	 try
+         {
+    		 image=ImageIO.read(new File("resources/images/fleets/fleet"+(i+1)+".png"));
+         }
+         catch (IOException e)
+         {
+             // TODO Auto-generated catch block
+             e.printStackTrace();
+         }
+      
+      	colors[i]=new Color(image.getRGB(image.getTileHeight()/2, image.getWidth()/2));
+     }
+    }
+    
     public Player(int number)
     {
         this(number, new Color(Universe.randomGenerator.nextInt(255),Universe.randomGenerator.nextInt(255),Universe.randomGenerator.nextInt(255)));
