a5eadab603368156ff666181e39b15a04e23ea3b
Ben Jeffrey <mail@benjeffrey.net>
2013-03-10 18:20:46 +0000

    new ComputerPlayer subclass of player, with changes made to Player for attr access.

---
+++ ComputerPlayer.java
@@ -0,0 +1,20 @@
+import java.awt.Color;
+
+
+
+public class ComputerPlayer extends Player
+{
+
+    public ComputerPlayer(int number)
+    {
+        super(number);
+        // TODO Auto-generated constructor stub
+    }
+
+    public ComputerPlayer(int number, Color color)
+    {
+        super(number, color);
+        // TODO Auto-generated constructor stub
+    }
+
+}
--- Player.java
+++ Player.java
@@ -12,40 +12,40 @@ import javax.imageio.ImageIO;
  */
 public class Player
 { 
-    private int number;
-    private Color color;
+    protected int number;
+    protected Color color;
+
+
+    public static Color[] colors;
+    public static int numColors;
+    protected static int nextColor=0;
+
+    static
+    {
+        numColors=18;
+        colors=new Color[numColors];
+        int i;
+        Color t;
+        BufferedImage image = null;
+        for(i=0;i<numColors;i++)
+        {
+            try
+            {
+                image=ImageIO.read(new File("resources/images/fleets/fleet"+(i+1)+".png"));
+            }
+            catch (IOException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            colors[i]=new Color(image.getRGB(image.getTileHeight()/2, image.getWidth()/2));
+        }
+    }
 
     /**
      * Create a new player, with the given number and a random colour.
      */
-    
-    public static Color[] colors;
-    public static int numColors;
-    private static int nextColor=0;
-    
-    static
-    {
-     numColors=18;
-     colors=new Color[numColors];
-     int i;
-     Color t;
-     BufferedImage image = null;
-     for(i=0;i<numColors;i++)
-     {
-    	 try
-         {
-    		 image=ImageIO.read(new File("resources/images/fleets/fleet"+(i+1)+".png"));
-         }
-         catch (IOException e)
-         {
-             // TODO Auto-generated catch block
-             e.printStackTrace();
-         }
-      
-      	colors[i]=new Color(image.getRGB(image.getTileHeight()/2, image.getWidth()/2));
-     }
-    }
-    
     public Player(int number)
     {
         this(number, colors[nextColor++]);
