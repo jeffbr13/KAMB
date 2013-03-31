8e2451cd482cc89463ff57255fa701f6b2f55ede
Ben Jeffrey <mail@benjeffrey.net>
2013-04-01 00:49:00 +0100

    Removed misleading comment

--- Planet.java
+++ Planet.java
@@ -126,11 +126,6 @@ public class Planet extends GamePiece
         }
     }*/
 
-    /**
-     * @return a boolean stating whether or not the given coordinate is 'inside' the
-     * planet. Can be used for testing mouse-click-selection.
-     */
-
     public double distance(Planet p)
     {
         return Math.sqrt((xCenter-p.getXCenter())*(xCenter-p.getXCenter())+(yCenter-p.getYCenter())*(yCenter-p.getYCenter()));
